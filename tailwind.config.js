8a7c5f14b7b4793c27098910aba961438894725a
Talal Agha <atikbhuiyan.bd@gmail.com>
2021-06-19 14:03:51 +0600

    Stater File

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'custom-blue' : '#304292',
+      }
+    },
+    fontFamily:{
+      'lato' : ['Lato', 'sans-serif'],
+      'noto' : ['Noto Sans KR', 'sans-serif'],
+    },
+    fontSize:{
+      'csm':'0.813rem'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
